833c2bd5a413df4ad0d6bc7bd12f4775a394ea7b
Andrew Masih <armasih@outlook.com>
2021-05-05 00:12:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var apple = "apple";
+var apple = "banana";
+
+let banana = "banana";
+let banana = "apple";
+
+/* let and var operate differntly with regard to scope, let will not let your re-delcare a veribale, where var will. themain difference is that var is scoped to the enclosed function (and any sub blocks)  */
